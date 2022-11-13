71ef1792c75d6c9716f3a537811e0228d67fbb66
leopauld ndenko <lndenko18@gmail.com>
2022-11-13 19:18:21 +0400

    files updated

--- main.js
+++ main.js
@@ -1,9 +1,25 @@
+// Date regex for 2 numbers
+// /^\d{2}$/
 const inputs= document.querySelectorAll('input[type="text"] ,input[type="number"],input[type="year"]');
 // console.log(inputs);
 //fonction that controls each input
 const nameChecker=(value)=>{
-  console.log(value);
-  
+  // console.log(value);
+  const nameContainer= document.querySelector('.name-container');
+  // console.log(nameContainer);
+  const errorName= document.querySelector('.name-container>span');
+  // console.log(errorName);
+  if (value.length>0 && (!value.match(/^[A-Z ]*$/))) {
+    nameContainer.classList.add('error');
+    errorName.textContent="Your name should contain any special character";
+    
+  // }else if(!value.match(/^[a-zA-Z0-9_.-]*$/)){
+  //   nameContainer.classList.add("error");
+  //   errorName.textContent="Your name should contain any special character";
+  }else{
+    nameContainer.classList.remove('error');
+    errorName.textContent="";
+  }
 }
 const numberChecker=(value)=>{
 console.log(value);
